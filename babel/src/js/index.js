9a7f65bc272a8423bf7ab28470fe1039e8784e2a
Carlos Andrés Aguilar Nieto <desagneto@gmail.com>
2020-02-03 14:11:21 -0500

    clase soporte de javascript moderno 1

---
+++ babel/src/js/index.js
@@ -0,0 +1,11 @@
+import '../css/index.css'
+import text from './text';
+
+if (module.hot){
+    module.hot.accept('./text.js', function () {
+        console.log('recargado caliente');
+        text;
+    })
+}
+
+text;
